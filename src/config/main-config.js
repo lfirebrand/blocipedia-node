8a77f23a1aa74d83619a247ed0fa7b398123365e
Lexi Zgud <lexibitesblog@gmail.com>
2018-10-09 21:12:37 -0500

    Debugging app.js and main-config.js

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
+const path = require("path");
+const logger = require("morgan");
 
 module.exports = {
-    init(app) {
+    init(app, express) {
+        app.use(express.static(path.join(__dirname, "..", "assets")));
+        app.use(logger('dev'));
     }
 };
