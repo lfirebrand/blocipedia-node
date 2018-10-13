fe6a89d2a9ce1c36c19478fba1fd660f9d5e5a9e
Lexi Zgud <lexibitesblog@gmail.com>
2018-10-12 20:29:24 -0500

    Debugging Blocipedia signup

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -6,13 +6,14 @@ describe("routes : static", () => {
 
     describe("GET /", () => {
 
-        it("should return status code 200", (done) => {
+        it("should return status code 200 and have 'Welcome to Blocipedia' in the body of the response", (done) => {
 
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
+                expect(body).toContain("Welcome to Blocipedia");
 
-                //#4
                 done();
+            
             });
         });
 
