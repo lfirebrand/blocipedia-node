f755a752eeb4036d1a2198dbebdb14f3722a4475
Lexi Zgud <lexibitesblog@gmail.com>
2018-10-17 21:01:22 -0500

    Debugging User Sign_In Sign_Out

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const validation = require("./validation");
 const userController = require("../controllers/userController");
-const User = require('../../src/db/models').User;
 
 router.get("/users/signup", userController.signUp);
 router.post("/users", validation.validateUsers, userController.create);
+router.get("/users/sign_in", userController.signInForm);
+router.post("/users/sign_in", validation.validateUsers, userController.signIn);
+router.get("/users/sign_out", userController.signOut);
 
 module.exports = router;
