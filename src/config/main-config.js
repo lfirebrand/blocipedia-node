323432fbc8fcbf472ab2fd82329756cf13d1576f
Lexi Zgud <lexibitesblog@gmail.com>
2018-10-18 19:55:44 -0500

    Flash debugging

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -2,19 +2,19 @@ require("dotenv").config();
 const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
 const bodyParser = require("body-parser");
-const logger = require("morgan");
-const passportConfig = require("./passport-config");
 const expressValidator = require("express-validator");
 const session = require("express-session");
 const flash = require("express-flash");
-
+const passportConfig = require("./passport-config");
+const logger = require("morgan");
 module.exports = {
     init(app, express) {
         app.set("views", viewsFolder);
         app.set("view engine", "ejs");
-        app.use(bodyParser.urlencoded( { extended: true }));
+        app.use(bodyParser.urlencoded({
+            extended: true
+        }));
         app.use(express.static(path.join(__dirname, "..", "assets")));
-        app.use(logger('dev'));
         app.use(expressValidator());
         app.use(session({
             secret: process.env.cookieSecret,
@@ -26,9 +26,11 @@ module.exports = {
         }));
         app.use(flash());
         passportConfig.init(app);
+
         app.use((req, res, next) => {
             res.locals.currentUser = req.user;
             next();
         })
+        app.use(logger("dev"));
     }
 };
