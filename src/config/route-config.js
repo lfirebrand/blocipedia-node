4490ce94579e662a8146c673e7b2a7f4863853dd
Lexi Zgud <lexibitesblog@gmail.com>
2018-10-23 23:15:13 -0500

    Implemented User Roles and Auth

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,6 +2,12 @@ module.exports = {
     init(app) {
         const staticRoutes = require("../routes/static");
         const userRoutes = require("../routes/users");
+
+        if (process.env.NODE_ENV === "test") {
+            const mockAuth = require("../../spec/support/mock-auth.js");
+            mockAuth.fakeIt(app);
+        }
+
         const wikiRoutes = require("../routes/wikis");
         app.use(staticRoutes);
         app.use(userRoutes);
